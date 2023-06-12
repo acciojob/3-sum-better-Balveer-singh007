5f0abb02a0bdb7d5836a20d6711867fba776ffde
Balveer Singh <128572901+Balveer-singh007@users.noreply.github.com>
2023-06-13 00:19:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	for(let i = 0 ; i<arr.length; i++){
+		if(i===target){
+			targetindex =i;
+			break;
+		}
+	}
+	let sumvalue1 = arr[targetindex-1];
+	let sumvalue2 = arr[targetindex];
+    let sumvalue3 = arr[targetindex+1];
+	let ans = (sumvalue1+sumvalue2+sumvalue3);
+	return ans;
 }
 
 module.exports = threeSum;
