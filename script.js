f4d797e4bd5d288557e0438e1f07103db52b7b82
Balveer Singh <128572901+Balveer-singh007@users.noreply.github.com>
2023-06-13 23:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 function threeSum(arr, target) {
 // write your code here
-	let flag = false;
-	for(let i = 0 ; i<arr.length; i++){
-		if(i===target){
-			targetindex =i;
-			break;
-		}
-	}
-	let sumvalue1 = arr[targetindex-1];
-	let sumvalue2 = arr[targetindex];
-    let sumvalue3 = arr[targetindex+1];
-	let ans = (sumvalue1+sumvalue2+sumvalue3);
-	return ans;
-	
-}
+	// let flag = false;
+	// for(let i = 0 ; i<arr.length; i++){
+	// 	if(i===target){
+	// 		targetindex =i;
+	// 		break;
+	// 	}
+	// }
+	// let sumvalue1 = arr[targetindex-1];
+	// let sumvalue2 = arr[targetindex];
+ //    let sumvalue3 = arr[targetindex+1];
+	// let ans = (sumvalue1+sumvalue2+sumvalue3);
+	// return ans;
+
+	var diff = Number.MAX_VALUE;
+        var ans = 0;
+
+        for(let i = 0; i < nums.length; i++){
+            for(let j = i + 1; j < nums.length; j++){
+                for(let k = j + 1; k < nums.length; k++){
+                    let sum = nums[i]  + nums[j] + nums[k];
+
+                    if(Math.abs(sum - target) < diff){
+                        diff = Math.abs(sum - target);
+                        ans = sum;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
 
 module.exports = threeSum;
